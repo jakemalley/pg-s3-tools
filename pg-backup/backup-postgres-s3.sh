0af9bd044374ad3da897d9462538a6c50fec492d
Jake Malley <jja.malley@gmail.com>
2019-03-28 18:35:53 +0000

    Changed script to use environment variables!

--- pg-backup/backup-postgres-s3.sh
+++ pg-backup/backup-postgres-s3.sh
@@ -3,29 +3,10 @@
 ## Backup a Postgres database using pg_dump and ship
 ## to an S3 object store.
 
-# 
-# The following environment variables are used:
-# PGDUMP_PREFIX - Sets the Prefix of the dumped file.
-# PGDATABASE    - Used by pg_dump
-# PGHOST        - Used by pg_dump
-# PGPORT        - Used by pg_dump
-# PGUSER        - Used by pg_dump
-# PGPASSWORD    - Used by pg_dump
-#
-# S3CONFIG      - Used by s3cmd
-#               - Defaults to ~/.s3cfg
-# S3BUCKET      - Used by s3cmd
-
 if [ -z "$PGDUMP_PREFIX" ]; then
     DUMP_PREFIX="pg_dump"
 else
     DUMP_PREFIX="$PGDUMP_PREFIX"
-fi
-
-if [ -z "$S3CONFIG" ]; then
-    S3_CONFIG_ARG=""
-else
-    S3_CONFIG_ARG="-c ${S3CONFIG}"
 fi
 
 DUMP_TIMESTAMP=`date +%Y%m%d_%H%M%S`
@@ -34,6 +15,38 @@ DUMP_FILE_NAME="${DUMP_PREFIX}_${DUMP_TIMESTAMP}.pgdump"
 # Simple log function with the date.
 echo_log () {
     echo "`date`: $*"
+}
+
+# Validate environment variables
+validate() {
+    if [ -z $PGDATABASE ]; then
+        echo_log "Failed to start backup: env PGDATABASE not set"
+    fi
+
+    if [ -z $S3_HOST ]; then
+        echo_log "Failed to start backup: S3_HOST not set"
+        exit 1
+    fi
+
+    if [ -z $S3_HOST_BUCKET ]; then
+        echo_log "Failed to start backup: S3_HOST_BUCKET not set"
+        exit 1
+    fi
+
+    if [ -z $S3_BUCKET ]; then
+        echo_log "Failed to start backup: S3_BUCKET not set"
+        exit 1
+    fi
+
+    if [ -z $AWS_ACCESS_KEY_ID ]; then
+        echo_log "Failed to start backup: AWS_ACCESS_KEY_ID not set"
+        exit 1
+    fi
+
+    if [ -z $AWS_SECRET_ACCESS_KEY ]; then
+        echo_log "Failed to start backup: AWS_SECRET_ACCESS_KEY not set"
+        exit 1
+    fi
 }
 
 # Use pg_dump to dump the database.
@@ -63,35 +76,29 @@ compress_pg_dump() {
 
 # Upload the .pgdump.gz file to the object store.
 upload_pg_dump() {
-    echo_log "Uploading to object store: s3://${S3BUCKET}/${DUMP_FILE_NAME}.gz"
-    if s3cmd put ${S3_CONFIG_ARG} ${DUMP_FILE_NAME}.gz s3://${S3BUCKET}; then
-        echo_log "Uploaded to object store successfully: s3://${S3BUCKET}/${DUMP_FILE_NAME}.gz"
+    echo_log "Uploading to object store: s3://${S3_BUCKET}/${DUMP_FILE_NAME}.gz"
+    if s3cmd put ${S3CMD_ARG} ${DUMP_FILE_NAME}.gz s3://${S3_BUCKET}; then
+        echo_log "Uploaded to object store successfully: s3://${S3_BUCKET}/${DUMP_FILE_NAME}.gz"
         return 0
     else
-        echo_log "Failed to upload to object store: s3://${S3BUCKET}/${DUMP_FILE_NAME}.gz"
+        echo_log "Failed to upload to object store: s3://${S3_BUCKET}/${DUMP_FILE_NAME}.gz"
         exit 1
     fi
 }
 
-if [ ! -z $PGDATABASE ] && [ ! -z $S3BUCKET ]; then
+echo_log "Starting backup of database ${PGDATABASE}"
 
-    echo_log "Starting backup of database ${PGDATABASE}"
-    
-    # Run the backup
-    create_pg_dump
-    compress_pg_dump
-    upload_pg_dump
+# Validate
+validate
 
-    # If we didn't exit 1; then we have successfully uploaded.
-    echo_log "Backup of database ${PGDATABASE} completed successfully"
-    exit 0
+# Build s3cmd Args
+S3CMD_ARG="--host=${S3_HOST} --host-bucket=${S3_HOST_BUCKET}"
 
-else
-    if [ -z $PGDATABASE ]; then
-        echo_log "Failed to start backup: env PGDATABASE not set"
-    fi
-    if [ -z $S3BUCKET ]; then
-        echo_log "Failed to start backup: env S3BUCKET not set"
-    fi
-    exit 1
-fi
+# Run the backup
+create_pg_dump
+compress_pg_dump
+upload_pg_dump
+
+# If we didn't exit 1; then we have successfully uploaded.
+echo_log "Backup of database ${PGDATABASE} completed successfully"
+exit 0
